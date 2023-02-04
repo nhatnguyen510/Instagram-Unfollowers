1b4d2d7802bde2dac5e59b5f8059d91f3d83771a
Nguyễn Ngọc Minh Nhật <71200617+nhatnguyen510@users.noreply.github.com>
2023-02-04 10:53:47 +0700

    Configure user route

--- server/src/routes/logoutRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { logout } from "../controllers/userController.js";
-
-const logoutRoute = Router();
-
-logoutRoute.post("/", logout);
-
-export default logoutRoute;
--- server/src/routes/unfollowersRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { findUnfollowers } from "../controllers/userController.js";
-
-const unfollowersRoute = Router();
-
-unfollowersRoute.get("/", findUnfollowers);
-
-export default unfollowersRoute;
